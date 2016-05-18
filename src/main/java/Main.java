2542fcdec9ac41f406fe2a6bb70f9d1b9555a544
Robert Gu <robertgu@Roberts-MacBook-Pro.local>
2016-05-18 15:05:51 -0700

    Make the game playable from homepage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,7 +17,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Router router = new ResourceRouter();
         Configuration config = new Configuration(router);
-        config.addRoute("/player-vs-player", new PlayerVsPlayerController(new ResourceReader(), new TicTacToeResponseBuilder()));
+        config.addRoute("/", new PlayerVsPlayerController(new ResourceReader(), new TicTacToeResponseBuilder()));
         config.addRoute("/game-won", new GameStatusController(new TicTacToeGameStatus(), new TicTacToeResponseBuilder(), new JsonConverter(), new TicTacToeJsonBuilder()));
         config.addRoute("/computer-move", new ComputerMoveController(new TicTacToeService(), new TicTacToeResponseBuilder(), new JsonConverter(), new TicTacToeJsonBuilder()));
         config.setRoutes();
